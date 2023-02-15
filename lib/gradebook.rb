fe48ef0d7af61260478ccacea64928d04d191d47
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-15 10:12:06 -0700

    add tests for grades across all courses and students in grade range

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,1 +1,30 @@
+class Gradebook
+  attr_reader :courses
 
+  def initialize
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    student_hash = Hash.new("Course doesn't exist")
+    @courses.each do |course|
+      student_hash[course] = course.students
+    end
+    student_hash
+  end
+
+  def students_below(threshold)
+    struggling_students = []
+    @courses.each do |course|
+      course.students.each do |student|
+        struggling_students << student if student.grade < threshold
+      end
+    end
+    struggling_students
+  end
+
+end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe Gradebook do
     @gradebook = Gradebook.new
     @course_1 = Course.new("Baking", 2)
     @course_2 = Course.new("Juggling", 3)
-    @student_1 = Student.new("A", 11)
+    @student_1 = Student.new({name: "A", grade: 11})
     @student_1.log_score(100)
     @student_1.log_score(90)
-    @student_2 = Student.new("B", 22)
+    @student_2 = Student.new({name: "B", grade: 22})
     @student_2.log_score(90)
     @student_2.log_score(80)
-    @student_3 = Student.new("C", 33)
+    @student_3 = Student.new({name: "C", grade: 33})
     @student_3.log_score(80)
     @student_3.log_score(70)
-    @student_4 = Student.new("D", 44)
+    @student_4 = Student.new({name: "D", grade: 44})
     @student_4.log_score(70)
     @student_4.log_score(60)
-    @student_5 = Student.new("E", 55)
+    @student_5 = Student.new({name: "E", grade: 55})
     @student_5.log_score(60)
     @student_5.log_score(50)
     @course_1.enroll(@student_1)
@@ -34,17 +34,29 @@ RSpec.describe Gradebook do
 
   it "exists" do
     expect(@gradebook).to be_a(Gradebook)
-    expect(@gradebook.courses).to eq(["Baking","Juggling"])
+    expect(@gradebook.courses).to eq([@course_1, @course_2])
   end
 
   it "can list all students in courses" do
-    expect(@gradebook.list_all_students).to eq({"Baking" => ["A", "B"] "Juggling" => ["C", "D", "E"]})
+    expect(@gradebook.list_all_students).to be_a(Hash)
+    expect(@gradebook.list_all_students[@course_1]).to be_a(Array)
+    expect(@gradebook.list_all_students[@course_1].count).to eq(2)
   end
 
   it "can list students below grade threshold" do
     expect(@gradebook.students_below(70).count).to eq(2)
-    expect(@gradebook.students_below(70)[0].name).to eq("D")
-    expect(@gradebook.students_below(70)[1].name).to eq("E")
+    expect(@gradebook.students_below(70)[0]).to eq(@student_4)
+    expect(@gradebook.students_below(70)[1]).to eq(@student_5)
+  end
+
+  it "can track all grades across all courses" do
+    expect(@gradebook.all_grades).to be_a(Hash)
+    expect(@gradebook.all_grades[@course_1][students][@student_1]).to eq(95.0)
+    expect(@gradebook.all_grades[@course_2][students][@student_4]).to eq(65.0)
+  end
+
+  it "can return all students within a certain grade range" do
+    expect(@gradebook.students_in_range(60, 80)).to eq([student_3, student_4])
   end
 
 end
