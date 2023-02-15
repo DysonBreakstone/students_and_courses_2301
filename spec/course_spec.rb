76e23e6cc668d88d6a9c78248c7eaf3e07b14d70
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-15 09:30:53 -0700

    course exists and has name, capacity, and students attributs, can enroll students, and determine if full

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -16,15 +16,26 @@ RSpec.describe Course do
   end
 
   it "has an array of students which can be enrolled, and can fill course" do
-    expect(course.students).to eq([])
-    expect(course.full?).to eq(false)
+    expect(@course.students).to eq([])
+    expect(@course.full?).to eq(false)
 
     student_1 = Student.new({name: "Morgan", age: 21})
     student_2 = Student.new({name: "Jordan", age: 29})
-    course.enroll(student1)
-    course.enroll(student2)
+    @course.enroll(student_1)
+    @course.enroll(student_2)
 
-    expect(course.students.count).to eq(2)
-    expect(course.full?).to eq(true)
+    expect(@course.students.count).to eq(2)
+    expect(@course.full?).to eq(true)
+  end
+
+  it "cannot enroll more students than capacity" do
+    student_1 = Student.new({name: "Morgan", age: 21})
+    student_2 = Student.new({name: "Jordan", age: 29})
+    student_3 = Student.new({name: "Petunia", age: 93})
+    @course.enroll(student_1)
+    @course.enroll(student_2)
+    @course.enroll(student_3)
+    
+    expect(@course.students.count).to eq(2)
   end
 end
