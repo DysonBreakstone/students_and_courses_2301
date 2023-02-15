a691e014818bdbb990ca6932c6249def38515b8f
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-15 09:18:34 -0700

    add name, age, and score attributes to Student class, as well as log_score and grade methods.

--- lib/student.rb
+++ lib/student.rb
@@ -1,1 +1,18 @@
+class Student
 
+  attr_reader :name, :age, :scores
+
+  def initialize(info_hash)
+    @name = info_hash[:name]
+    @age = info_hash[:age]
+    @scores = []
+  end
+
+  def log_score(new_score)
+    @scores << new_score
+  end
+
+  def grade
+    @scores.sum.to_f / @scores.count.to_f
+  end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,29 +1,29 @@
 require './lib/student'
 
-RSPEC.describe Student do
+RSpec.describe Student do
   before(:each) do
-    student = Student.new({name: "Morgan", age: 21})
+    @student = Student.new({name: "Morgan", age: 21})
   end
 
   it "exists" do
-    expect(student).to be_a(Student)
+    expect(@student).to be_a(Student)
   end
   
   it "has a name" do
-    expect(student.name).to eq("Morgan")
+    expect(@student.name).to eq("Morgan")
   end
 
-  it "has the correct age attribute") do
-    expect(student.age).to eq(21)
+  it "has the correct age attribute" do
+    expect(@student.age).to eq(21)
   end
 
   it "possesses a scores attribute and log_score method and average score" do
-    expect(student.scores).to eq([])
+    expect(@student.scores).to eq([])
 
-    student.log_score(89)
-    student.log_score(78)
+    @student.log_score(89)
+    @student.log_score(78)
 
-    expect(student.scores).to eq([89,78])
-    expect(student.grade).to eq(83.5)
-    end
+    expect(@student.scores).to eq([89,78])
+    expect(@student.grade).to eq(83.5)
+  end
 end
